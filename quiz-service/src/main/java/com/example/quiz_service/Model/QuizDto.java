ac5b81eb95b8134868549a9b515cb70f472e1b5f
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-20 23:42:39 +0530

    mdified QuizDto file

--- quiz-service/src/main/java/com/example/quiz_service/Model/QuizDto.java
+++ quiz-service/src/main/java/com/example/quiz_service/Model/QuizDto.java
@@ -4,8 +4,31 @@ import lombok.Data;
 
 @Data
 public class QuizDto {
-    String category;
-    int num;
-    String title;
+    private String category;
+    private int num;
+    private String title;
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }

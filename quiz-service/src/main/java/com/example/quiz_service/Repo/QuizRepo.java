4664f3bd9ca729df10f65dd6e57b50568f87b382
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-20 18:33:15 +0530

    initialized server registry

--- quiz-service/src/main/java/com/example/quiz_service/Repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.quiz_service.Repo;
-
-import com.example.quiz_service.Model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz,Integer> {
-}

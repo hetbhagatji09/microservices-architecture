2abea635885ce8862633a3b5b43d849413e2de54
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-12-20 18:34:04 +0530

    delete the questions model

---
+++ quiz-service/src/main/java/com/example/quiz_service/Model/Quiz.java
@@ -0,0 +1,33 @@
+package com.example.quiz_service.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ quiz-service/src/main/java/com/example/quiz_service/Model/QuizDto.java
@@ -0,0 +1,11 @@
+package com.example.quiz_service.Model;
+
+import lombok.Data;
+
+@Data
+public class QuizDto {
+    String category;
+    int num;
+    String title;
+
+}
